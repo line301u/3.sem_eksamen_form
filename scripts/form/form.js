4612df0f8cb715da9f241ba7dd04e46551d274f3
Linea <line301u@stud.kea.dk>
2021-06-14 12:30:31 +0200

    validation feedback styling

--- scripts/form/form.js
+++ scripts/form/form.js
@@ -10,34 +10,19 @@ function init() {
   setExpiryDateMask();
   document.querySelector(".pay").addEventListener("click", checkTextValidity);
 
-  showButtonWhenValid();
+  checkFormValidity();
   showPaymentAmount();
   removeDefaultValidation();
-  setValidityRules();
+
+  //prevent transtion on load
+  setTimeout(() => {
+    document.querySelector("body").classList.remove("preload");
+  }, 1000);
 }
 
 function removeDefaultValidation() {
+  //remove default validation
   document.querySelector("form").setAttribute("novalidate", true);
-}
-
-function setValidityRules() {
-  // const cardholder = document.querySelector("#cardholder_name");
-  // const cardnumber = document.querySelector("#cardnumber");
-  // const expiry_date = document.querySelector("#expiry_date");
-  // const cvv = document.querySelector("#cvv");
-  // cardnumber.onblur = function () {
-  //   if (cardnumber.validity.tooShort) {
-  //     cardnumber.nextElementSibling.textContent = "too short";
-  //   } else {
-  //     cardnumber.nextElementSibling.textContent = "Please fill out the correct information";
-  //   }
-  // };
-  // expiry_date.onblur = function () {
-  //   console.log(expiry_date.validity);
-  //   if (expiry_date.validity.tooShort) {
-  //     expiry_date.nextElementSibling.textContent = "too short";
-  //   }
-  // };
 }
 
 function showPaymentAmount() {
@@ -46,14 +31,28 @@ function showPaymentAmount() {
   document.querySelector(".payment_amount").textContent = `${paymentAmount},-`;
 }
 
-function showButtonWhenValid() {
+function checkFormValidity() {
   document.querySelectorAll("form input").forEach((input) => {
-    input.addEventListener("keyup", checkFormValidity);
+    input.addEventListener("keyup", showWhenValid);
 
-    function checkFormValidity() {
+    //show button when valid
+    function showWhenValid() {
       if (document.querySelector("form").checkValidity()) {
         document.querySelector(".pay").style.opacity = 1;
+      } else {
+        document.querySelector(".pay").style.opacity = 0.2;
       }
+
+      input.addEventListener("blur", () => {
+        //add valid symbol when valid
+        if (input.checkValidity()) {
+          this.nextElementSibling.classList.add("drawn");
+          console.log(this.nextElementSibling);
+          //remoce valid symbol when valid
+        } else {
+          this.nextElementSibling.classList.remove("drawn");
+        }
+      });
     }
   });
 }
@@ -69,24 +68,29 @@ async function checkTextValidity() {
 
     window.location.href = `process.html?id=${orderid}`;
   } else {
-    document.querySelectorAll("input").forEach((element) => {
-      console.log(element.validity);
-
-      if (element.validity.valueMissing) {
-        element.nextElementSibling.style.display = "block";
-
-        element.addEventListener("keyup", checkInput);
-
-        function checkInput() {
-          if (!element.validity.valueMissing) {
-            element.nextElementSibling.style.display = null;
-          }
-        }
-      }
-    });
-
+    addInvaldidStyling();
     console.log("form is not valid");
   }
+}
+
+function addInvaldidStyling() {
+  document.querySelectorAll("input").forEach((inputValue) => {
+    console.log(inputValue);
+    //value missing -> add styling
+    if (inputValue.validity.valueMissing) {
+      inputValue.nextElementSibling.nextElementSibling.nextElementSibling.style.display = "block";
+      inputValue.style.boxShadow = "0 0 0 1px tomato";
+
+      inputValue.addEventListener("keyup", checkInput);
+      //value not missing -> remove styling
+      function checkInput() {
+        if (!inputValue.validity.valueMissing) {
+          inputValue.nextElementSibling.nextElementSibling.nextElementSibling.style.display = null;
+          inputValue.style.boxShadow = null;
+        }
+      }
+    }
+  });
 }
 
 async function postOrder() {
